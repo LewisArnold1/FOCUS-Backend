588aacc1a317ea046bc9ade766a0386a56412fa3
LewisArnold1 <185296117+LewisArnold1@users.noreply.github.com>
2024-11-08 14:32:15 +0000

    Refactor WebSocket consumer to video_stream folder in eye_processing app

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -12,7 +12,8 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
-from backend import consumers
+from eye_processing.video_stream.consumers import VideoFrameConsumer
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
@@ -20,7 +21,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter([
-            path('ws/video/', consumers.VideoFrameConsumer.as_asgi()),  # This is the WebSocket URL
+            path('ws/video/', VideoFrameConsumer.as_asgi()),  # This is the WebSocket URL
         ])
     ),
 })
