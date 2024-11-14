12d09deadcc851d6d3bcda2d22f05f3b0dbdc916
LewisArnold1 <185296117+LewisArnold1@users.noreply.github.com>
2024-11-14 19:39:49 +0000

    Video frame / blink data now saves to sqlite db

--- backend/eye_processing/video_stream/consumers.py
+++ backend/eye_processing/video_stream/consumers.py
@@ -6,6 +6,7 @@ from PIL import Image
 import numpy as np
 import cv2
 from eye_processing.blink_detection.count_blinks import process_blink
+from datetime import datetime
 
 class VideoFrameConsumer(WebsocketConsumer):
 
@@ -26,23 +27,24 @@ class VideoFrameConsumer(WebsocketConsumer):
             self.process_frame(frame_data, timestamp)
 
     def process_frame(self, frame_data, timestamp):
-        # Decode the base64-encoded image
-        image_data = base64.b64decode(frame_data.split(',')[1])
-        image = Image.open(BytesIO(image_data))
-        frame = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+        try:
+            from eye_processing.models import SimpleEyeMetrics
+            # Decode the base64-encoded image
+            image_data = base64.b64decode(frame_data.split(',')[1])
+            image = Image.open(BytesIO(image_data))
+            frame = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-        # Call the blink detection function with the frame
-        total_blinks, ear = process_blink(frame)
+            # Call the blink detection function with the frame
+            total_blinks, ear = process_blink(frame)
 
-        # Print or send the results (e.g., to the frontend or console)
-        print(f"Timestamp: {timestamp}, Total Blinks: {total_blinks}, EAR: {ear}")
+            # Print or send the results (e.g., to the frontend or console)
+            # Convert the timestamp from milliseconds to a datetime object
+            timestamp_s = timestamp / 1000
+            timestamp_dt = datetime.fromtimestamp(timestamp_s)
+            print(f"Timestamp: {timestamp_dt}, Total Blinks: {total_blinks}, EAR: {ear}")
 
-        # If you want to send results back to the frontend
-        '''
-        self.send(text_data=json.dumps({
-            'timestamp': timestamp,
-            'total_blinks': total_blinks,
-            'ear': ear
-        
-        }))
-        '''
+            # Save the metrics for this frame in the database
+            eye_metrics = SimpleEyeMetrics(timestamp=timestamp_dt, blink_count=total_blinks, eye_aspect_ratio=ear,)
+            eye_metrics.save()
+        except (base64.binascii.Error, PIL.UnidentifiedImageError) as e:
+            print("Error decoding image:", e)
