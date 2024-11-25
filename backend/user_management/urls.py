73f05ea8332ac0f423958f1ba24edce00e8aff7b
LewisArnold1 <185296117+LewisArnold1@users.noreply.github.com>
2024-11-25 21:35:44 +0000

    Fix: Removed redundant import

--- backend/user_management/urls.py
+++ backend/user_management/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterUserView, UserProfileView
+from .views import RegisterUserView
 
 # User-specific api end-points so when a user visits register or profile, django routes request to appropriate user management views
 urlpatterns = [
