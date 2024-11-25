5102609ae8b986122e29f11a72790dfa25526040
LewisArnold1 <185296117+LewisArnold1@users.noreply.github.com>
2024-11-25 21:32:57 +0000

    Fix: Simplified login form to include only username and password

--- backend/user_management/urls.py
+++ backend/user_management/urls.py
@@ -4,5 +4,4 @@ from .views import RegisterUserView, UserProfileView
 # User-specific api end-points so when a user visits register or profile, django routes request to appropriate user management views
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
-    path('profile/', UserProfileView.as_view(), name='profile'),
 ]
--- backend/user_management/views.py
+++ backend/user_management/views.py
@@ -1,16 +1,9 @@
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import generics
-from .serializers import UserDisplaySerializer, RegisterUserSerializer
+from .serializers import RegisterUserSerializer
 from django.contrib.auth.models import User
 
 class RegisterUserView(generics.CreateAPIView):
     queryset = User.objects.all() # Ensure user does not already exist
-    serializer_class = RegisterUserSerializer # Requirred data to create user
+    serializer_class = RegisterUserSerializer # Required data to create user
     permission_classes = [AllowAny] # Any user should be able to register
-
-class UserProfileView(generics.RetrieveAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = UserDisplaySerializer
-
-    def get_object(self):
-        return self.request.user
