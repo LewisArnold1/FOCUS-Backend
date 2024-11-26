d849c5991e4c0b40ecc1cc679b06c1347bfc0124
Zakariya Lilani <zaklilani@gmail.com>
2024-11-26 12:03:44 +0000

    sends api/token to new view

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+'''
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+'''
+from rest_framework_simplejwt.views import (TokenRefreshView)
+from .views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), #login endpoint
+    #path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), #login endpoint
+    path('api/token/', LoginView.as_view(), name='login'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api-auth/', include("rest_framework.urls")),
     path('api/user/', include('user_management.urls')),
