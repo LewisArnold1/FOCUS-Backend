13311362db2ac84ef7760ee4611a91a56ca83a5c
Zakariya Lilani <zaklilani@gmail.com>
2024-11-27 17:03:48 +0000

    add back in user serializer

--- backend/user_management/serializers.py
+++ backend/user_management/serializers.py
@@ -15,3 +15,7 @@ class RegisterUserSerializer(serializers.ModelSerializer):
         validated_data['password'] = make_password(validated_data['password'])
         return super(RegisterUserSerializer, self).create(validated_data)
 
+class UserDisplaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
