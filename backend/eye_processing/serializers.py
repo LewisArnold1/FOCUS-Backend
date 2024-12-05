8f2d345fc2f80618b20b94837daa242859185b76
LewisArnold1 <185296117+LewisArnold1@users.noreply.github.com>
2024-12-05 21:31:00 +0000

    Removed simple eye metrics serializer

--- backend/eye_processing/serializers.py
+++ backend/eye_processing/serializers.py
@@ -1,13 +1,7 @@
 from rest_framework import serializers
-from .models import SimpleEyeMetrics
 
 class VideoDataSerializer(serializers.Serializer):
     video_frame = serializers.ImageField()
 
     class Meta:
         fields = ['video_frame']
-
-class SimpleEyeMetricsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SimpleEyeMetrics
-        fields = ['timestamp', 'blink_count', 'eye_aspect_ratio']
