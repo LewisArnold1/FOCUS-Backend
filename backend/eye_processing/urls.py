32ce15ae6e74815ec1c7b6deed0c20b6eaf5d90c
LewisArnold1 <185296117+LewisArnold1@users.noreply.github.com>
2024-12-05 22:33:57 +0000

    Updated blink count end point

--- backend/eye_processing/urls.py
+++ backend/eye_processing/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RetrieveLastBlinkCountView, RetrieveAllUserSessionsView
 
 urlpatterns = [
-    path('api/last-blink-count/', RetrieveLastBlinkCountView.as_view(), name='last-blink-count'),
+    path('last-blink-count/', RetrieveLastBlinkCountView.as_view(), name='last-blink-count'),
     path('reading-times/', RetrieveAllUserSessionsView.as_view(), name='reading-times'),
 ]
