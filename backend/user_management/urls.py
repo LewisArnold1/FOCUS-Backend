c6745a7fc7e9ae38ef1ce6d360468276467fcb47
LewisArnold1 <185296117+LewisArnold1@users.noreply.github.com>
2024-12-12 16:47:17 +0000

    Added profile endpoint

--- backend/user_management/urls.py
+++ backend/user_management/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterUserView, CalibrationView, CalibrationRetrievalView
+from .views import RegisterUserView, ProfileView, CalibrationView, CalibrationRetrievalView
 
 # User-specific api end-points so when a user visits register or profile, django routes request to appropriate user management views
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile'),
     path('calibrate/', CalibrationView.as_view(), name='calibrate'),
     path('calibration-retrieval/', CalibrationRetrievalView.as_view(), name='calibration-retrieval'),
 ]
