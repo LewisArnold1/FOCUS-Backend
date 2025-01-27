e045282e76a66824cd79ade27686540b55286427
Soniya Aryal <73301897+sonj20@users.noreply.github.com>
2025-01-27 12:10:56 +0000

    Polynomial regression
    applied using polynomial feature

--- backend/eye_processing/eye_metrics/predict_blink_count.py
+++ backend/eye_processing/eye_metrics/predict_blink_count.py
@@ -1,11 +1,12 @@
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
 def predict_blink_count(user):
     from eye_processing.models import SimpleEyeMetrics
     # Retrieve all blink counts and sort by timestamp
     metrics = SimpleEyeMetrics.objects.filter(user=user).order_by('timestamp')
 
-    if len(metrics) < 1:  #atleast number of datapoints
+    if len(metrics) < 10:  #atleast number of datapoints
         print("Not enough data to predict the next blink count.")
         return None
 
@@ -17,11 +18,15 @@ def predict_blink_count(user):
     X = [[lag] for lag in blink_lags[1:]]  # Lag 
     y = blink_counts[1:]  # Target is the current blink count
 
+     # Transform features to polynomial features
+    poly = PolynomialFeatures(degree=2)  # Adjust degree as needed
+    X_poly = poly.fit_transform(X)
     
     model = LinearRegression()
-    model.fit(X, y)
+    model.fit(X_poly, y)
 
     # Predict the next blink count using the last blink count as input
-    predicted_blink_count = model.predict([[blink_counts[-1]]])[0]
+    last_blink_count_poly = poly.transform([[blink_counts[-1]]])
+    predicted_blink_count = model.predict(last_blink_count_poly)[0]
 
     return predicted_blink_count
