68c2e314d2f6331519b51dee44acf376dbbeb4b5
Zakariya Lilani <zaklilani@gmail.com>
2025-03-01 17:15:44 +0000

    added testing prints

--- backend/eye_processing/eye_metrics/process_blinks.py
+++ backend/eye_processing/eye_metrics/process_blinks.py
@@ -29,6 +29,16 @@ def process_blinks(ear_values, timestamps, middle_frame_timestamp):
     if len(timestamps) != len(ear_values):
         print('Issue with process_blinks: timestamps & EAR unequal length')
         return False
+    
+    time_period = (max(timestamps) - min(timestamps)).total_seconds()
+    fps = len(timestamps)/time_period
+
+    # Check >= 12 FPS
+    if fps < 12:
+        print('FPS too low')
+        return False
+    
+    # print(f"Time period recieved: {(max(timestamps) - min(timestamps)).total_seconds()}")
 
     # Identify centre EAR & timestamp
     centre_index = timestamps.index(middle_frame_timestamp)
@@ -41,72 +51,47 @@ def process_blinks(ear_values, timestamps, middle_frame_timestamp):
     end_time = centre_time + timedelta(seconds=0.35)
     before_indices = [i for i in range(len(timestamps)) if timestamps[i] >= start_time and timestamps[i] < centre_time]
     after_indices = [i for i in range(len(timestamps)) if timestamps[i] <= end_time and timestamps[i] > centre_time]
+    
+    # print('\n before changes')
 
-    # Require equal number of frames either side of centre frame
-    while len(before_indices) !=len(after_indices):
-        # Remove any unequal frames
-        if len(before_indices) > len(after_indices):
-            before_indices = before_indices[1:]
-        else:
-            after_indices = after_indices[:-1]
+    # print(f'before indices: {before_indices}')
+    # print(f'after indices: {after_indices}')
+
+    # # Require equal number of frames either side of centre frame
+    # while len(before_indices) !=len(after_indices):
+    #     print('not exactly 30 fps')
+    #     # Remove any unequal frames
+    #     if len(before_indices) > len(after_indices):
+    #         before_indices = before_indices[1:]
+    #     else:
+    #         after_indices = after_indices[:-1]
 
     # Find frames before & after centre
     before_EARs = [ear_values[i] for i in before_indices]
     after_EARs = [ear_values[i] for i in after_indices]
     
-    '''remove'''
-    if len(before_indices) != len(after_indices):
-        print('removing frames incorrectly')
-        return False
-    
-    # Check >= 11.4 FPS
-    if len(before_indices) < 4:
-        print('FPS too low')
-        return False
-    
+    # print(f'\nno. frames before padding: {len(before_EARs)+len(after_EARs)+1}')
+    # before = timestamps[before_indices[0]]
+    # after = timestamps[after_indices[-1]]
+    # print(f'time period {(after-before).total_seconds()}')
+    # print(f'Intended time period {(end_time-start_time).total_seconds()}')
+
+    # Pad or downsample so feature window has 10 frames either side of centre frame
     if len(before_indices) != 10:
-    # > 30 FPS or > 30 FPS
-        indices = np.linspace(0,len(before_EARs)-1, 10).astype(int)
-        before_EARs = [ear_values[i] for i in indices]
-        after_EARs = [ear_values[i] for i in indices]
+        before_indices = np.linspace(0,len(before_EARs)-1, 10).astype(int)
+        before_EARs = [ear_values[i] for i in before_indices]
+
+    if len(after_indices) != 10:
+        after_indices = np.linspace(0,len(after_EARs)-1, 10).astype(int)
+        after_EARs = [ear_values[i] for i in after_indices]
+
+    # print('after padding')
+    # print(f'before indices: {before_indices}')
+    # print(f'after indices: {after_indices}')
 
     feature_window = before_EARs + [centre_EAR] + after_EARs
 
-    '''
-    # Check if window has ~30fps: 25<fps<35 (0.65<t<0.84)
-    window = (timestamps[-1] - timestamps[0]).total_seconds()
-
-    valid_pairs = [(timestamps[i], ear_values[i]) for i in range(len(ear_values)) if ear_values[i] is not None]
-
-    if len(valid_pairs) < 5:
-        return False
-    
-    filtered_timestamps, filtered_ears = zip(*valid_pairs)
-    selected_ears = np.array(filtered_ears)
-    selected_times = np.array([(t - filtered_timestamps[0]).total_seconds() for t in filtered_timestamps])  # Convert to seconds
-
-    if len(selected_ears) > 21:
-        # Downsample: Select 21 evenly spaced frames
-        downsample_indices = np.linspace(0, len(selected_ears) - 1, 21).astype(int)
-        sampled_ears = selected_ears[downsample_indices]
-
-    elif len(selected_ears) < 21:
-        # Upsample: Use interpolation to create 21 points - MUST CHANGE!
-        interp_func = interp1d(selected_times, selected_ears, kind='linear', fill_value="extrapolate")
-        new_times = np.linspace(0, selected_times[-1], 21)  # Generate 21 equally spaced timestamps
-        sampled_ears = interp_func(new_times)  # Interpolated EAR values
-
-    else:
-        sampled_ears = selected_ears  # Already 21 frames
-
-    # Reshape for SVM prediction
-    sampled_ears = np.array(sampled_ears).reshape(1, -1)  # Convert to 2D array
-
-    # Scale and predict using the SVM model
-    X_scaled = scaler.transform(sampled_ears)
-    y_pred = svm_model.predict(X_scaled)
-
-    '''
+    # print(f"SVM input: {len(feature_window)}")
 
     '''remove'''
     if len(feature_window) != 21:
@@ -114,9 +99,11 @@ def process_blinks(ear_values, timestamps, middle_frame_timestamp):
         return False
     
     if any(EAR is None for EAR in feature_window):
+        print('None values present')
         return False
 
     if np.any(np.isnan(np.array(feature_window))):
+        print('NaN values present')
         return False
     
     feature_window = np.array(feature_window).reshape(1, -1)
@@ -125,6 +112,6 @@ def process_blinks(ear_values, timestamps, middle_frame_timestamp):
     y_pred = svm_model.predict(X_scaled)
 
     if y_pred == True:
-        print (f"predict: {y_pred}")
+        print (f"                  Blink predicted")
 
     return y_pred
